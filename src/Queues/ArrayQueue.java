8b0a2b87d0c17a8f557646e04188d3e62a0c3d08
Leonardo <leosud099@gmail.com>
2020-09-04 10:40:39 -0600

    Wrapping implementation done

--- src/Queues/ArrayQueue.java
+++ src/Queues/ArrayQueue.java
@@ -15,20 +15,40 @@ public class ArrayQueue {
     public void add(Employee employee) {
 
         // If array is full create new array and double de capacity
-        // then copy over the old array to the new one - O(n)
-        if (back == queue.length) {
+        // then copy over the old array to the new one - O(n) and unwrap it
+        if (size() == queue.length - 1) {
+            int numItems = size();
             Employee[] newArray = new Employee[2 * queue.length];
-            System.arraycopy(queue, 0, newArray, 0, queue.length);
+
+            // Unwrap it then copy
+            System.arraycopy(queue, front, newArray, 0, queue.length - front);
+            System.arraycopy(queue, 0, newArray, queue.length - front, back);
             queue = newArray;
+
+            front = 0;
+            back = numItems;
         }
 
         // No resizing needed O(1)
         queue[back] = employee;
-        back++;
+
+        // Wrap it if needed
+        if (back < queue.length - 1) {
+            back++;
+        } else {
+            back = 0;
+        }
     }
 
     public int size() {
-        return back - front;
+        // No wrap occurred
+        if (front <= back) {
+            return back - front;
+        }
+        // Wrap occurred
+        else {
+            return back - front + queue.length;
+        }
     }
 
     public Employee remove() {
@@ -47,6 +67,8 @@ public class ArrayQueue {
         if (size() == 0) {
             front = 0;
             back = 0;
+        } else if (front == queue.length) {
+            front = 0;
         }
 
         return employee;
@@ -62,8 +84,19 @@ public class ArrayQueue {
     }
 
     public void printQueue() {
-        for (int i = front; i < back; i++) {
-            System.out.println(queue[i]);
+        // No wrap occurred
+        if (front <= back) {
+            for (int i = front; i < back; i++) {
+                System.out.println(queue[i]);
+            }
+        }
+        else{
+            for (int i = front; i < queue.length; i++) {
+                System.out.println(queue[i]);
+            }
+            for (int i = front; i < back; i++) {
+                System.out.println(queue[i]);
+            }
         }
     }
 }
